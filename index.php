2ad0e3bc8974fa5d288082e07e28431cc283bc45
Sodiqjon1998 <sirojiddinovsodiqjon1998@gmail.com>
2024-01-06 12:59:49 +0500

    sdvsvdsdsd

--- index.php
+++ index.php
@@ -5,5 +5,6 @@ include "Telegram.php";
 $telegram = new Telegram("6721406026:AAHO5AGgz3f4OZD_Z0nSofoISwr_-coWGJc");
 
 $chat_id = $telegram->ChatID();
-$content = array('chat_id' => $chat_id, 'text' => 'Test');
+$text = $telegram->Text();
+$content = array('chat_id' => $chat_id, 'text' => $text);
 $telegram->sendMessage($content);
