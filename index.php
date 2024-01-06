11f5c292078820fa691b82acb76cb986ebaa77df
Sodiqjon1998 <sirojiddinovsodiqjon1998@gmail.com>
2024-01-06 13:21:56 +0500

    svsvsds

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $telegram = new Telegram("6721406026:AAHO5AGgz3f4OZD_Z0nSofoISwr_-coWGJc");
 
 $chat_id = $telegram->ChatID();
 $text = $telegram->Text();
-if($text == "/start"){
+if($text == "/startbot"){
     $content = array('chat_id' => $chat_id, 'text' => "Salom botimizga hush kelibsiz!");
     $telegram->sendMessage($content);
 }
