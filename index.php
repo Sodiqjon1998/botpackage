8a904b28f02adcb47d4d8ea073574d7ff905c2dc
Sodiqjon1998 <sirojiddinovsodiqjon1998@gmail.com>
2024-01-07 08:50:22 +0500

    vsvsdvsdvsdd

--- index.php
+++ index.php
@@ -5,26 +5,19 @@
 $token = '6721406026:AAHO5AGgz3f4OZD_Z0nSofoISwr_-coWGJc';
 $chat_id = '-1002089884417'; // Foydalanuvchi chat_id
 
-// Xabar matni
-$message = "Salom, bu cURL orqali yuborilgan xabar!";
+// Webhookdan olingan ma'lumotlarni olish
+$update = json_decode(file_get_contents("php://input"), true);
 
-// cURL so'rovi tuzish
-$api_url = "https://api.telegram.org/bot$token/sendMessage";
-$post_fields = array(
-    'chat_id' => $chat_id,
-    'text' => $message
-);
+// Eng so'nggi yozilgan xabarlar
+foreach ($update['message']['text'] as $message) {
+    $user_id = $message['from']['id'];
+    $user_name = $message['from']['first_name'];
+    $text = $message['text'];
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $api_url);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-$result = curl_exec($ch);
-curl_close($ch);
-
-// Natija
-echo $result;
+    echo "User ID: $user_id\n";
+    echo "User Name: $user_name\n";
+    echo "Text: $text\n";
+    echo "-------------------------\n";
+}
 ?>
 
