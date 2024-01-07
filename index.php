8761844000d5b4a8be8795f3f3a1d3125abeed89
Sodiqjon1998 <sirojiddinovsodiqjon1998@gmail.com>
2024-01-07 08:46:59 +0500

    aasdcsdsvs

--- index.php
+++ index.php
@@ -1,30 +1,30 @@
 <?php
 
-define("API_KEY", "6721406026:AAHO5AGgz3f4OZD_Z0nSofoISwr_-coWGJc");
-
-$chatId = "-1002089884417";
-
-$telegramApiUrl = "https://api.telegram.org/bot".API_KEY."/sendMessage";
-
-$keyboard = [
-    'keyboard' => [
-        ['Button 1', 'Button 2'],
-        ['Button 3', 'Button 4'],
-    ],
-    'resize_keyboard' => true,
-    'one_time_keyboard' => true
-];
-
-$replyMarkup = json_encode($keyboard);
 
 
-$params = [
-    'chat_id' => $chatId,
-    "text" => "Salom",
-    "reply_markup" => $replyMarkup 
-];
+$token = '6721406026:AAHO5AGgz3f4OZD_Z0nSofoISwr_-coWGJc';
+$chat_id = '-1002089884417'; // Foydalanuvchi chat_id
 
-$response = file_get_contents($telegramApiUrl . '?' . http_build_query($params));
+// Xabar matni
+$message = "Salom, bu cURL orqali yuborilgan xabar!";
 
-echo "<pre>";
- print_r($response);
+// cURL so'rovi tuzish
+$api_url = "https://api.telegram.org/bot$token/sendMessage";
+$post_fields = array(
+    'chat_id' => $chat_id,
+    'text' => $message
+);
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $api_url);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$result = curl_exec($ch);
+curl_close($ch);
+
+// Natija
+echo $result;
+?>
+
