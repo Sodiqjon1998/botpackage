fbff85c7cc7107522e45f43c3f7730af35239897
Sodiqjon1998 <sirojiddinovsodiqjon1998@gmail.com>
2024-01-07 13:57:32 +0500

    sdvfvbdfsdsd

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
 
 
-$servername = "localhost"; // O'zgartiring
-    $username = "yuksali9_med"; // O'zgartiring
-    $password = "d9e[I2J0l=Pw"; // O'zgartiring
-    $dbname = "yuksali9_edu"; // O'zgartiring
+// $servername = "localhost"; // O'zgartiring
+//     $username = "yuksali9_med"; // O'zgartiring
+//     $password = "d9e[I2J0l=Pw"; // O'zgartiring
+//     $dbname = "yuksali9_edu"; // O'zgartiring
 
-    // MySQL bağlantısını yaratish
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
+//     // MySQL bağlantısını yaratish
+//     $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-    // Bağlanti tekshirish
-    if ($conn) {
-        echo "Successfly";
-    }
+//     // Bağlanti tekshirish
+//     if ($conn) {
+//         echo "Successfly";
+//     }
 
-    echo "Xatolik";
+//     echo "Xatolik";
 
-    
+
