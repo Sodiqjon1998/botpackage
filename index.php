d65d317ed7b83c5465196352d5f3872aa55dca81
Sodiqjon1998 <sirojiddinovsodiqjon1998@gmail.com>
2024-01-07 07:58:56 +0500

    sfdvsds

--- index.php
+++ index.php
@@ -6,9 +6,22 @@ $chatId = "-1002089884417";
 
 $telegramApiUrl = "https://api.telegram.org/bot".API_KEY."/sendMessage";
 
+$keyboard = [
+    'keyboard' => [
+        ['Button 1', 'Button 2'],
+        ['Button 3', 'Button 4'],
+    ],
+    'resize_keyboard' => true,
+    'one_time_keyboard' => true
+];
+
+$replyMarkup = json_encode($keyboard);
+
+
 $params = [
     'chat_id' => $chatId,
     "text" => "Salom",
+    "reply_markup" => $replyMarkup 
 ];
 
 $response = file_get_contents($telegramApiUrl . '?' . http_build_query($params));
