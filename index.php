a79b19e2bd3620e3aae5e9a1db0d19be58b4546c
Sodiqjon1998 <sirojiddinovsodiqjon1998@gmail.com>
2024-01-07 14:09:31 +0500

    sdsdvsd

--- index.php
+++ index.php
@@ -7,13 +7,12 @@ $password = "aS7X?uamuE]I"; // O'zgartiring
 $dbname = "yuksali9_edu"; // O'zgartiring
 
 // MySQL bağlantısını yaratish
-$conn = mysqli_connect($servername, $username, $password);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
 // Bağlanti tekshirish
 if (!$conn) {
     echo "Xatolik";
 }
 else{
-    
     echo "Zo'r";
 }
