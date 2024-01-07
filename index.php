3d2b8aa4597bdfb3f9ec4558304b3b272117d37e
Sodiqjon1998 <sirojiddinovsodiqjon1998@gmail.com>
2024-01-07 11:53:58 +0500

    sdvsvvdfvsdd

--- index.php
+++ index.php
@@ -1,36 +1,32 @@
 <?php
+// Telegram botning tokeni
+$token = "6721406026:AAHO5AGgz3f4OZD_Z0nSofoISwr_-coWGJc";
 
+// Telegram dan kelgan so'rovni olish
+$update = json_decode(file_get_contents("php://input"), TRUE);
 
-$chatId = "-1002089884417";
+// Kelgan so'rovni tekshirish
+if (isset($update["message"])) {
+    $chatId = $update["message"]["chat"]["id"];
+    $messageText = $update["message"]["text"];
 
-$token = '6721406026:AAHO5AGgz3f4OZD_Z0nSofoISwr_-coWGJc';
-$db_host = 'localhost';
-$db_user = 'root';
-$db_password = 'root';
-$db_name = 'bot';
+    // Qo'ng'iroqni shakllantirish
+    $responseText = "Sizning so'rovingiz: $messageText";
 
-// Telegramdan kelgan xabarni olish
-$update = json_decode(file_get_contents("php://input"), true);
-
-// Xabar ma'lumotlarini izlash
-$user_id = $update['message']['from']['id'];
-$user_name = $update['message']['from']['first_name'];
-$text = $update['message']['text'];
-
-// Bazaga saqlash
-$conn = new mysqli($db_host, $db_user, $db_password, $db_name);
-
-if ($conn->connect_error) {
-    die("Bazaga ulanishda xatolik: " . $conn->connect_error);
+    // Telegramga javob yuborish
+    sendMessage($chatId, $responseText, $token);
 }
 
-$sql = "INSERT INTO user (id, first_name, text) VALUES ($user_id, '$user_name')";
+// Telegramga javob yuborish uchun funksiya
+function sendMessage($chatId, $message, $token) {
+    $url = "https://api.telegram.org/bot$token/sendMessage";
+    $params = [
+        'chat_id' => $chatId,
+        'text' => $message,
+    ];
 
-if ($conn->query($sql) === TRUE) {
-    echo "Xabar ma'lumotlari bazaga muvaffaqiyatli saqlandi";
-} else {
-    echo "Xabar ma'lumotlari saqlashda xatolik: " . $conn->error;
+    $url = $url . '?' . http_build_query($params);
+    file_get_contents($url);
 }
 
-$conn->close();
 ?>
