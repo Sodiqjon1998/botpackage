d5c1a0f3766654124fcff1e00eb79f5aaec66bd5
Sodiqjon1998 <sirojiddinovsodiqjon1998@gmail.com>
2024-01-07 12:13:50 +0500

    svsdvsd

--- index.php
+++ index.php
@@ -1,35 +1,46 @@
-<?php
-// Telegram botning tokeni
-$token = "6721406026:AAHO5AGgz3f4OZD_Z0nSofoISwr_-coWGJc";
 
-// Telegram dan kelgan so'rovni olish
+
+
+<?php
+// Token va guruh ID sini o'zingizning botingiz ma'lumotlari bilan almashtiring
+$botToken = "6721406026:AAHO5AGgz3f4OZD_Z0nSofoISwr_-coWGJc";
+$chatId = -1002089884417; // Guruh ID
+
+// Telegramdan kelgan so'rovlar ma'lumotlari
 $update = json_decode(file_get_contents("php://input"), TRUE);
 
-// Kelgan so'rovni tekshirish
-if (isset($update["message"])) {
-    $chatId = $update["message"]["chat"]["id"];
-    $messageText = $update["message"]["text"];
-    $userId = $update['message']['from']['id'];
+// A'zolar ro'yxati
+$members = $update["message"]["new_chat_members"];
 
-    // Qo'ng'iroqni shakllantirish
-    $responseText = "Sizning so'rovingiz: $messageText";
+// A'zolikni tekshirish va xabar yuborish
+foreach ($members as $member) {
+    $user_id = $member["id"];
+    $user_name = $member["first_name"];
 
-    // Telegramga javob yuborish
-    sendMessage($chatId, $responseText, $token, $userId);
+    // A'zolikni tekshirish
+    $is_member = isMember($botToken, $chatId, $user_id);
+
+    // Xabarni yuborish
+    if (!$is_member) {
+        sendMessage($botToken, $chatId, "Assalomu alaykum, $user_name! Xush kelibsiz guruhga!");
+    }
 }
 
-// Telegramga javob yuborish uchun funksiya
-function sendMessage($chatId, $message, $token, $userId) {
+// A'zolikni tekshirish funksiyasi
+function isMember($token, $chat_id, $user_id) {
+    $url = "https://api.telegram.org/bot$token/getChatMember?chat_id=$chat_id&user_id=$user_id";
+    $result = json_decode(file_get_contents($url), TRUE);
+    return ($result["ok"] && $result["result"]["status"] == "member");
+}
+
+// Xabar yuborish funksiyasi
+function sendMessage($token, $chat_id, $text) {
     $url = "https://api.telegram.org/bot$token/sendMessage";
     $params = [
-        'chat_id' => $chatId,
-        'text' => $userId,
+        "chat_id" => $chat_id,
+        "text" => $text,
     ];
-
-    echo $userId;
-
-    $url = $url . '?' . http_build_query($params);
-    file_get_contents($url);
+    file_get_contents($url . '?' . http_build_query($params));
 }
-
 ?>
+
