d75e5912e8ed148ce0a2c6ae5abc9ee7b5d35f55
Sodiqjon1998 <sirojiddinovsodiqjon1998@gmail.com>
2024-01-25 23:00:32 +0500

    sdvsvsd

--- index.php
+++ index.php
@@ -1,45 +1,13 @@
 <?php
-/**
- * Telegram Bot Example whitout WebHook.
- * It uses getUpdates Telegram's API.
- *
- * @author Gabriele Grillo <gabry.grillo@alice.it>
- */
-include 'Telegram.php';
 
-$bot_token = '6721406026:AAHO5AGgz3f4OZD_Z0nSofoISwr_-coWGJc';
-$telegram = new Telegram($bot_token);
+$update = json_decode(file_get_contents('php://input'), true);
 
-// Get all the new updates and set the new correct update_id
-$req = $telegram->getUpdates();
-for ($i = 0; $i < $telegram->UpdateCount(); $i++) {
-    // You NEED to call serveUpdate before accessing the values of message in Telegram Class
-    $telegram->serveUpdate($i);
-    $text = $telegram->Text();
-    $chat_id = $telegram->ChatID();
 
-    if ($text == '/start') {
-        $reply = 'Working';
-        $content = ['chat_id' => $chat_id, 'text' => $reply];
-        $telegram->sendMessage($content);
-    }
-    if ($text == '/test') {
-        if ($telegram->messageFromGroup()) {
-            $reply = 'Chat Group';
-        } else {
-            $reply = 'Private Chat';
-        }
-        // Create option for the custom keyboard. Array of array string
-        $option = [['A', 'B'], ['C', 'D']];
-        // Get the keyboard
-        $keyb = $telegram->buildKeyBoard($option);
-        $content = ['chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => $reply];
-        $telegram->sendMessage($content);
-    }
-    if ($text == '/git') {
-        $reply = 'Check me on GitHub: https://github.com/Eleirbag89/TelegramBotPHP';
-        // Build the reply array
-        $content = ['chat_id' => $chat_id, 'text' => $reply];
-        $telegram->sendMessage($content);
-    }
-}
+$url = "https://api.telegram.org/bot6721406026:AAHO5AGgz3f4OZD_Z0nSofoISwr_-coWGJc/sendMessage";
+$params = [
+    'chat_id' => -1002089884417,
+    'text' => "Assalomu alaykum /start shu kabi yuboring!",
+];
+
+$url = $url . '?' . http_build_query($params);
+file_get_contents($url);
