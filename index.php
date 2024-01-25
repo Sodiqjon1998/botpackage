3fad7287c9b4c322eeba1937a97dd77b449cca2c
Sodiqjon1998 <sirojiddinovsodiqjon1998@gmail.com>
2024-01-25 23:15:09 +0500

    sdvsdvsdvs

--- index.php
+++ index.php
@@ -3,8 +3,32 @@
 $update = json_decode(file_get_contents('php://input'), true);
 
 
-echo "<pre>";
-print_r($update);
+$servername = "localhost";
+$username = "avisenam_ed";
+$password = "aJT4.Q?luLRs";
+$dbname = "avisenam_ed";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$username1 = $update['message']['from']['username'];
+$user_id = $update['message']['from']['id'];
+
+$sql = "INSERT INTO bot_users (user_id, first_name)
+VALUES ($user_id, $username1)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+
 
 $url = "https://api.telegram.org/bot6721406026:AAHO5AGgz3f4OZD_Z0nSofoISwr_-coWGJc/sendMessage";
 $params = [
