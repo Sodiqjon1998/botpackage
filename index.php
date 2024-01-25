01a1a7acbcd7c5990cffc68dc686cc9146a1dffe
Sodiqjon1998 <sirojiddinovsodiqjon1998@gmail.com>
2024-01-25 23:02:56 +0500

    sdvsdvsd

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 $update = json_decode(file_get_contents('php://input'), true);
 
 
+echo "<pre>";
+print_r($update);
+
 $url = "https://api.telegram.org/bot6721406026:AAHO5AGgz3f4OZD_Z0nSofoISwr_-coWGJc/sendMessage";
 $params = [
     'chat_id' => -1002089884417,
