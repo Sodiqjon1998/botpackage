8cb232565b1c66976ae8f7a4d0ad6caef5d4f3e1
Sodiqjon1998 <sirojiddinovsodiqjon1998@gmail.com>
2024-01-26 21:09:12 +0500

    sdvsvsdsvs

--- index.php
+++ index.php
@@ -18,31 +18,23 @@ define('API_KEY', '6721406026:AAHO5AGgz3f4OZD_Z0nSofoISwr_-coWGJc');
 
 
 
-function sendMessage(){
-  $url = "https://api.telegram.org/bot".API_KEY."/sendMessage";
-  global $url;
-  $update = json_decode(file_get_contents('php://input'), true);
+$url = "https://api.telegram.org/bot" . API_KEY . "/sendMessage";
 
-  $keyboard = [
-    "keyboard" => [
-      [["text" => "Checkbox 1", "request_contact" => true]],
-      [["text" => "Checkbox 2", "request_contact" => true]],
-      [["text" => "Yuborish"]],
-    ],
-    "resize_keyboard" => true,
-    "one_time_keyboard" => true,
-  ];
-  $encodedKeyboard = json_encode($keyboard);
-  $params = [
-    'chat_id' => '-1002089884417',
-    'text' => "Assalomu alaykum /start shu kabi yuboring!",
-    'reply_markup' => $encodedKeyboard
-  ];
-  
-  $url = $url . '?' . http_build_query($params);
-  echo file_get_contents($url);
-}
+$keyboard = [
+  "keyboard" => [
+    [["text" => "Checkbox 1", "request_contact" => true]],
+    [["text" => "Checkbox 2", "request_contact" => true]],
+    [["text" => "Yuborish"]],
+  ],
+  "resize_keyboard" => true,
+  "one_time_keyboard" => true,
+];
+$encodedKeyboard = json_encode($keyboard);
+$params = [
+  'chat_id' => '-1002089884417',
+  'text' => "Assalomu alaykum /start shu kabi yuboring!",
+  'reply_markup' => $encodedKeyboard
+];
 
-sendMessage();
-
-
+$url = $url . '?' . http_build_query($params);
+file_get_contents($url);
