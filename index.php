529f2026521bfc925c62339c0c044493f83b93ee
Sodiqjon1998 <sirojiddinovsodiqjon1998@gmail.com>
2024-01-26 20:59:04 +0500

    sdvsvsd

--- index.php
+++ index.php
@@ -3,6 +3,14 @@ require_once "config.php";
 
 $update = json_decode(file_get_contents('php://input'), true);
 
+$sql = "INSERT INTO bot_users (user_id, first_name, text)
+VALUES ('{$update['message']['from']['id']}', '{$update['message']['from']['username']}', '{$update['message']['text']}')";
+
+if (mysqli_query($conn, $sql)) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
 
 mysqli_close($conn);
 
