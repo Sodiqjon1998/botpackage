ac0403ea8f3ba9a715b0c422796ce8241cfb81d0
sirojidinov17 <sirojidinov17@gmail.com>
2024-01-27 11:25:12 +0500

    vvvvvvvvvvvvvvvvvvvvvvvvv

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
- include "config.php";
+
+define("API_KEY", '6721406026:AAHO5AGgz3f4OZD_Z0nSofoISwr_-coWGJc');
+
+$servername = "localhost";
+$username = "avisenam_ed";
+$password = "aJT4.Q?luLRs";
+$dbname = "avisenam_ed";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
 
 
 
