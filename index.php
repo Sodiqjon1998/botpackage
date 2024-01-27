02d19f3958059c40ae45a26ae19348d097cecbad
Sodiqjon1998 <sirojiddinovsodiqjon1998@gmail.com>
2024-01-27 20:16:56 +0500

    sdvsdvsvsd

--- index.php
+++ index.php
@@ -37,24 +37,4 @@ echo sendMessage("sendMessage", $params);
 
 
 
-// SQL query to select all rows from the 'bot_users' table
-$sql1 = "SELECT * FROM bot_users";
-
-// Execute the query
-$result = mysqli_query($conn, $sql1);
-
-// Check if any rows were returned
-if (mysqli_num_rows($result) > 0) {
-    // Output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        echo $row['username'];
-    }
-} else {
-    echo "0 results";
-}
-
-// Close the connection
-mysqli_close($conn);
-
-
 
