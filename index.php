0af0f6e1560ded92a8897edba0d9e0fcc36679d4
sirojidinov17 <sirojidinov17@gmail.com>
2024-01-27 13:41:34 +0500

    sdvsvsdvs

--- index.php
+++ index.php
@@ -47,9 +47,9 @@ function sendMessage(){
   ];
   $encodedKeyboard = json_encode($keyboard);
   $params = [
-    'chat_id' => $update['message']['from']['id'],
-    // 'chat_id' => $update['message']['chat']['id'],
-    'chat_id' => -1002089884417,
+    // 'chat_id' => $update['message']['from']['id'],
+    'chat_id' => $update['message']['chat']['id'],
+    // 'chat_id' => -1002089884417,
     'text' => "Assalomu alaykum /start shu kabi yuboring!",
     // 'reply_markup' => $encodedKeyboard
   ];
