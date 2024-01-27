7e51e7151d94e4ee8a988bfb8fd40d5d90edc795
sirojidinov17 <sirojidinov17@gmail.com>
2024-01-27 13:42:11 +0500

    svsdvsvds

--- index.php
+++ index.php
@@ -51,7 +51,7 @@ function sendMessage(){
     'chat_id' => $update['message']['chat']['id'],
     // 'chat_id' => -1002089884417,
     'text' => "Assalomu alaykum /start shu kabi yuboring!",
-    // 'reply_markup' => $encodedKeyboard
+    'reply_markup' => $encodedKeyboard
   ];
   
   $url = $url . '?' . http_build_query($params);
