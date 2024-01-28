9af39e604505848b3b77ca8915779b0e57194fa5
Sodiqjon1998 <sirojiddinovsodiqjon1998@gmail.com>
2024-01-28 16:39:02 +0500

    bbbb

--- function.php
+++ function.php
@@ -1,9 +1,10 @@
 <?php
 include "config.php";
 
-function dump($what){
-    echo '<pre>'; 
-        print_r($what); 
+function dump($what)
+{
+    echo '<pre>';
+    print_r($what);
     echo '</pre>';
 };
 
@@ -97,24 +98,23 @@ function check($update)
 
 
     if (mysqli_num_rows($result) > 0) {
+        if ($update['message']['text'] == "/start") {
+            echo kickUser($chat_id, $update['message']['message_id']);
+            echo sendMessage("sendMessage", $check);
+            $sql = "INSERT INTO bot_users (user_id, first_name, username, is_bot, language_code)
+            VALUES (
+            '{$update['message']['from']['id']}', 
+            '{$update['message']['from']['first_name']}', '{$update['message']['from']['username']}', '{$update['message']['from']['is_bot']}', '{$update['message']['from']['language_code']}')";
 
-    } elseif ($update['message']['text'] == "/start") {
-        echo kickUser($chat_id, $update['message']['message_id']);
-        echo sendMessage("sendMessage", $check);
-        $sql = "INSERT INTO bot_users (user_id, first_name, username, is_bot, language_code)
-        VALUES (
-        '{$update['message']['from']['id']}', 
-        '{$update['message']['from']['first_name']}', '{$update['message']['from']['username']}', '{$update['message']['from']['is_bot']}', '{$update['message']['from']['language_code']}')";
-
-        if (mysqli_query($conn, $sql)) {
-            echo "New record created successfully";
-        } else {
-            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+            if (mysqli_query($conn, $sql)) {
+                echo "New record created successfully";
+            } else {
+                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+            }
+        } elseif ($update['message']['text'] == "/work") {
+            echo sendMessageReply("sendMessage", $params);
         }
-    }elseif($update['message']['text'] == "/work"){
-        echo sendMessageReply("sendMessage", $params);
-    }
-     else {
+    } else {
         echo sendMessage("sendMessage", $notes);
     }
 }
