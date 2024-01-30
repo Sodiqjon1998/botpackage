365ceeea63f255483699abcee512cded2b202f78
Sodiqjon1998 <sirojiddinovsodiqjon1998@gmail.com>
2024-01-30 22:26:16 +0500

    sdsvsdvsvsds

--- function.php
+++ function.php
@@ -75,10 +75,12 @@ function check($update)
 
     // Convert the keyboard markup array to JSON
     $replyMarkup = json_encode($keyboard);
+    if(isset($update['callback_query'])){
 
+    }
     $params = [
         'chat_id' => $update['message']['from']['id'],
-        'text' => "@" . $update['message']['from']['username'] . " " . $update['message']['from']['first_name'] . " " . $update['message']['from']['last_name'],
+        'text' => "@" . $update['message']['from']['username'] . " " . $update['message']['from']['first_name'] . " " . $update['message']['from']['last_name'] . $update['callback_query']['data'],
         'reply_markup' => $replyMarkup,
     ];
 
@@ -100,18 +102,6 @@ function check($update)
     if (mysqli_num_rows($result) > 0) {
         if ($update['message']['text'] == "/work") {
             echo sendMessageReply("sendMessage", $params);
-            dump(sendMessageReply("sendMessage", $params));
-        } elseif ($update["callback_query"]["data"] == "submit") {
-            $sql = "INSERT INTO bot_users (user_id, first_name, username, is_bot, language_code)
-            VALUES (
-            '{$update['message']['from']['id']}', 
-            '{$update['callback_query']['data']}', '{$update['message']['from']['username']}', '{$update['message']['from']['is_bot']}', '{$update['message']['from']['language_code']}')";
-
-            if (mysqli_query($conn, $sql)) {
-                echo "New record created successfully";
-            } else {
-                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-            }
         } else {
         }
     } elseif ($update['message']['text'] == "/start") {
