f9ce33c20c2662c04937079920aad6de4c3d5f39
Sodiqjon1998 <sirojiddinovsodiqjon1998@gmail.com>
2024-01-30 21:04:57 +0500

    sdvsdvs

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 include "config.php";
 include "function.php";
 
-header("location:users.php");
+
 $update = json_decode(file_get_contents('php://input'), true);
 
 $url = "https://api.telegram.org/bot" . API_KEY . "/sendMessage";
 
 echo check($update);
 
-
+header("location:users.php");
 
